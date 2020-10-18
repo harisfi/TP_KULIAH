4e1a15acf6da8fc9a1552921fe42cd2f8fa45b8a
Haris Firmansyah <acc.cp.haris@gmail.com>
2020-10-18 21:13:26 +0700

    Add file
    Tugas3a

---
+++ Semester1/TP-03/Tugas3a.java
@@ -0,0 +1,7 @@
+public class Tugas3a {
+    public static void main(String[] args) {
+        int HPM = 500000, P = 12, L = 8, totalHarga;
+        totalHarga = P*L*HPM;
+        System.out.println("Total harga tanah untuk luas : "+(P*L)+" adalah Rp"+totalHarga);
+    }
+}
