be61a91f54299ec5512141400d12d9465a581805
Haris Firmansyah <acc.cp.haris@gmail.com>
2020-10-18 21:13:55 +0700

    Add file
    Tugas 3c

---
+++ Semester1/TP-03/Tugas3c.java
@@ -0,0 +1,11 @@
+public class Tugas3c {
+    public static void main(String[] args) {
+        int a, b, x, y, z;
+        a = 4;
+        b = 6;
+        x = b*4;
+        y = a*2;
+        z = (a*x)+(b*y*y);
+        System.out.println("Nilai x : "+x+"\nNilai y : "+y+"\nNilai z : "+z);
+    }
+}
