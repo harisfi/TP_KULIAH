8ddaea512676a9f612aa5924265220a685a2e8af
harisfi <acc.cp.haris@gmail.com>
2020-11-16 09:48:30 +0700

    Update praktik2.java

--- Semester1/TP-07/praktik2.java
+++ Semester1/TP-07/praktik2.java
@@ -1,12 +1,19 @@
+
+import java.util.Scanner;
+
 /**
  *
  * @author hryzx
  */
 public class praktik2 {
     public static void main(String[] args) {
-        int hasil = 0;
+        int hasil = 0, nilaiMax;
+        Scanner in = new Scanner(System.in);
         
-        for (int i = 1; i <= 6; i++) {
+        System.out.println("Masukkan batas nilai maksimum:");
+        nilaiMax = in.nextInt();
+        
+        for (int i = 1; i <= nilaiMax; i++) {
             if (i % 2 != 0) {   // ganjil
                 System.out.print("+" + i);
                 hasil += i;
