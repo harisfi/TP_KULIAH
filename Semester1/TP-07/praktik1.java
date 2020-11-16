5ed599f73e44f554e204d802b0f92d5b8f4eef91
harisfi <acc.cp.haris@gmail.com>
2020-11-16 09:38:14 +0700

    Add file
    praktik1.java

---
+++ Semester1/TP-07/praktik1.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author hryzx
+ */
+public class praktik1 {
+    public static void main(String[] args) {
+        for (int i = 1; i >= 10; i++) {
+//        for (int i = 1; i <= 10; i+=2) {
+//        for (int i = 10; i > 1; i--) {
+//        for (int i = 10; i < 1; i++) {
+//        for(;;)
+//            System.out.print("a");
+            System.out.print(i + " ");
+        }
+    }
+}
