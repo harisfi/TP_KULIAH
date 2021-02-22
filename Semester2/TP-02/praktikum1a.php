51aa60387616e1ac620e5d3327b59d87907c413d
harisfi <acc.cp.haris@gmail.com>
2021-02-22 10:45:30 +0700

    Add file to Semester2/TP-02/
    Berisi jawaban latihan soal 4.5

---
+++ Semester2/TP-02/praktikum1a.php
@@ -0,0 +1,8 @@
+<?php
+    $nama = "Muhammad Haris Firmansyah";
+    $usia = 18;
+    $ipk = 3.5;
+
+    $format = "Nama saya %s, usia saya %d, dan memiliki ipk %.2f";
+    printf($format, $nama, $usia, $ipk);
+?>
