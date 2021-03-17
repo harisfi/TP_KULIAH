519f859a4fceafed129b0b7f5e57019855f80845
harisfi <acc.cp.haris@gmail.com>
2021-03-17 07:28:47 +0700

    Add file
    Minyak.java in SMT2-PBO/TP-03

---
+++ SMT2-PBO/TP-03/Minyak.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3;
+
+/**
+ *
+ * @author hryzx
+ */
+public class Minyak {
+    public void cetakKonversi() {
+        System.out.println("Minyak tanah diganti tabung gassss\n");
+    }
+}
