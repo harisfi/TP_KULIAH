2a40302eda093d107047c2780903a3337469549f
harisfi <acc.cp.haris@gmail.com>
2021-03-17 07:28:17 +0700

    Add file
    Club.java in SMT2-PBO/TP-03

---
+++ SMT2-PBO/TP-03/Club.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3;
+
+/**
+ *
+ * @author hryzx
+ */
+public class Club {
+    String clubName;
+    String pelatih;
+    
+    public String getClub() {
+        return clubName;
+    }
+
+    public String getPelatih() {
+        return pelatih;
+    }
+}
