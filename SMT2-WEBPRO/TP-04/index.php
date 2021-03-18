8a54de6d67de713ecc7a35b215123af956acbb14
harisfi <acc.cp.haris@gmail.com>
2021-03-18 22:17:35 +0700

    Add file
    index.php in SMT2-WEBPRO/TP-04

---
+++ SMT2-WEBPRO/TP-04/index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once("koneksi.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Latihan 8</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+</head>
+<?php
+    session_start();
+    if (!empty($_SESSION['nama']) && !empty($_SESSION['email']) && !empty($_SESSION['username'])) {
+        include("profil.php");
+    } else {
+        session_destroy();
+        include("form.php");
+    }
+?>
+</html>
