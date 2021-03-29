cfd58d6dd5ffd49f7d0e6c9dc9ca816f7beac74a
harisfi <acc.cp.haris@gmail.com>
2021-03-29 08:31:30 +0700

    Add file
    Perempuan.java in SMT2-PBO/TP-05/_1

---
+++ SMT2-PBO/TP-05/_1/Perempuan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas5._1;
+
+/**
+ *
+ * @author hryzx
+ */
+public class Perempuan extends Manusia{
+    public double HtgBBI(double tinggiBadan) {
+        return (tinggiBadan-100) * 0.8;
+    }
+}
