594e88d03a7d7e7fe2650ed30a06c2025aa6ac25
harisfi <acc.cp.haris@gmail.com>
2021-03-29 08:32:38 +0700

    Add file
    BangunDatar.java in SMT2-PBO/TP-05/_3

---
+++ SMT2-PBO/TP-05/_3/BangunDatar.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas5._3;
+
+/**
+ *
+ * @author hryzx
+ */
+public class BangunDatar {
+    public double luas() {
+        return 0;
+    }
+}
