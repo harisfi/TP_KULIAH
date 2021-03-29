44ad0305df8f0cba2be964d30e43efd81511e2bb
harisfi <acc.cp.haris@gmail.com>
2021-03-29 08:31:45 +0700

    Add file
    Employee.java in SMT2-PBO/TP-05/_2

---
+++ SMT2-PBO/TP-05/_2/Employee.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas5._2;
+
+/**
+ *
+ * @author hryzx
+ */
+public class Employee {
+    String nama;
+    int salary;
+
+    Employee() {
+    }
+
+    Employee(String nm) {
+        this.nama = nm;
+        System.out.println("Employee");
+    }
+    
+    public int salary() {
+        return 0;
+    }
+}
