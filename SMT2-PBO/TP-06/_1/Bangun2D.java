a8939b08a8eb3a7deadf3b618ac7fc2e546f21d1
harisfi <acc.cp.haris@gmail.com>
2021-04-16 09:31:09 +0700

    Add file
    Bangun2D.java in SMT2-PBO/TP-06/_1

---
+++ SMT2-PBO/TP-06/_1/Bangun2D.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas6._1;
+
+/**
+ *
+ * @author hryzx
+ */
+public interface Bangun2D {
+    double hitungLuas();
+    double hitungKeliling();
+}
