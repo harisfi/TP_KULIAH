1fd2c1b7dd5b691ca770812c26a8c470e3c19c6c
harisfi <acc.cp.haris@gmail.com>
2021-05-30 22:58:55 +0700

    Add file
    Student.java in SMT2-PBO/TP-08/app/db

---
+++ SMT2-PBO/TP-08/app/db/Student.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.db;
+
+/**
+ *
+ * @author hryzx
+ */
+public class Student {
+    private String nim;
+    private String name;
+    private String address;
+    private String email;
+    
+    public Student() {
+        
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
